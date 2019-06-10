d634d6d4e1da2a1fbb4682df633656372ac58ad1
Mayank Patel <mayankpatel@mayanks-mbp.mynet>
2019-06-10 21:06:21 +0100

    Start up code is added for the sentiment analysis

---
+++ robot-model/src/main/java/com/share/investment/SentimentAnalysis/DetectLanguage.java
@@ -0,0 +1,121 @@
+package com.share.investment.SentimentAnalysis;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *     groupId: com.google.code.gson
+ *     artifactId: gson
+ *     version: 2.8.1
+ *
+ * Once you have compiled or downloaded gson-2.8.1.jar, assuming you have placed it in the
+ * same folder as this file (DetectLanguage.java), you can compile and run this program at
+ * the command line as follows.
+ *
+ * Execute the following two commands to build and run (change gson version if needed):
+ * javac DetectLanguage.java -classpath .;gson-2.8.1.jar -encoding UTF-8
+ * java -cp .;gson-2.8.1.jar DetectLanguage
+ */
+
+class Document {
+    public String id, text;
+
+    public Document(String id, String text){
+        this.id = id;
+        this.text = text;
+    }
+}
+
+class Documents {
+    public List<Document> documents;
+
+    public Documents() {
+        this.documents = new ArrayList<Document>();
+    }
+    public void add(String id, String text) {
+        this.documents.add (new Document (id, text));
+    }
+}
+
+public class DetectLanguage {
+
+// ***********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+    // Replace the accessKey string value with your valid access key.
+    static String accessKey = "ab4f0bc85d184d078e2aa0078094ecea";
+
+// Replace or verify the region.
+
+// You must use the same region in your REST API call as you used to obtain your access keys.
+// For example, if you obtained your access keys from the uksouth region, replace
+// "westcentralus" in the URI below with "uksouth".
+
+    // NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+// a free trial access key, you should not need to change this region.
+    static String host = "https://uksouth.api.cognitive.microsoft.com";
+
+    static String path = "/text/analytics/v2.1/languages";
+
+    public static String GetLanguage (Documents documents) throws Exception {
+        String text = new Gson().toJson(documents);
+        byte[] encoded_text = text.getBytes("UTF-8");
+
+        URL url = new URL(host+path);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "text/json");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", accessKey);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.write(encoded_text, 0, encoded_text.length);
+        wr.flush();
+        wr.close();
+
+        StringBuilder response = new StringBuilder ();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(json_text).getAsJsonObject();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main (String[] args) {
+        try {
+            Documents documents = new Documents ();
+            documents.add ("1", "This is a document written in English.");
+            documents.add ("2", "Este es un document escrito en Español.");
+            documents.add ("3", "这是一个用中文写的文件");
+
+            String response = GetLanguage (documents);
+            System.out.println (prettify (response));
+        }
+        catch (Exception e) {
+            System.out.println (e);
+        }
+    }
+}
---
+++ robot-model/src/main/java/com/share/investment/SentimentAnalysis/GetSentiment.java
@@ -0,0 +1,126 @@
+package com.share.investment.SentimentAnalysis;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *     groupId: com.google.code.gson
+ *     artifactId: gson
+ *     version: 2.8.1
+ *
+ * Once you have compiled or downloaded gson-2.8.1.jar, assuming you have placed it in the
+ * same folder as this file (GetSentiment.java), you can compile and run this program at
+ * the command line as follows.
+ *
+ * Execute the following two commands to build and run (change gson version if needed):
+ * javac GetSentiment.java -classpath .;gson-2.8.1.jar -encoding UTF-8
+ * java -cp .;gson-2.8.1.jar GetSentiment
+ */
+
+
+//@todo commented below class check it later on
+class DocumentForSentimentAnalysis {
+    public String id, language, text;
+
+    public DocumentForSentimentAnalysis(String id, String language, String text){
+        this.id = id;
+        this.language = language;
+        this.text = text;
+    }
+}
+
+class DocumentsForSentimentAnalysis {
+    public List<DocumentForSentimentAnalysis> documents;
+
+    public DocumentsForSentimentAnalysis() {
+        this.documents = new ArrayList<DocumentForSentimentAnalysis>();
+    }
+    public void add(String id, String language, String text) {
+        this.documents.add (new DocumentForSentimentAnalysis(id, language, text));
+    }
+}
+
+public class GetSentiment {
+
+// ***********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+    // Replace the accessKey string value with your valid access key.
+    static String accessKey = "ab4f0bc85d184d078e2aa0078094ecea";
+
+// Replace or verify the region.
+
+// You must use the same region in your REST API call as you used to obtain your access keys.
+// For example, if you obtained your access keys from the uksouth region, replace
+// "westcentralus" in the URI below with "uksouth".
+
+    // NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+// a free trial access key, you should not need to change this region.
+    static String host = "https://uksouth.api.cognitive.microsoft.com";
+
+    static String path = "/text/analytics/v2.0/sentiment";
+
+    public static String getTheSentiment (DocumentsForSentimentAnalysis documents) throws Exception {
+        String text = new Gson().toJson(documents);
+        byte[] encoded_text = text.getBytes("UTF-8");
+
+        URL url = new URL(host+path);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "text/json");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", accessKey);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.write(encoded_text, 0, encoded_text.length);
+        wr.flush();
+        wr.close();
+
+        StringBuilder response = new StringBuilder ();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(json_text).getAsJsonObject();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main (String[] args) {
+        try {
+            DocumentsForSentimentAnalysis documents = new DocumentsForSentimentAnalysis();
+            documents.add ("1", "en", "I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.");
+            documents.add ("2", "es", "Este ha sido un dia terrible, llegué tarde al trabajo debido a un accidente automobilistico.");
+            documents.add ("3", "en", "MSFT looking good today ready to invest in it");
+            documents.add ("4", "en", "don't invest in Apple now");
+
+            String response = getTheSentiment (documents);
+            System.out.println (prettify (response));
+        }
+        catch (Exception e) {
+            System.out.println (e);
+        }
+    }
+}
